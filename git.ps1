448dc4207b6e15988c5cbb76d3986a0b792760ab
523881d815c8e2103631e610b4427c57 <116864491+523881d815c8e2103631e610b4427c57@users.noreply.github.com>
2024-01-10 09:56:34 +0800

    Create git.ps1

---
+++ git.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/523881d815c8e2103631e610b4427c57/MOA/main/All-In-One-Version/MOA.cmd'
+$DownloadURL2 = 'https://bitbucket.org/moa0x3a/microsoft-office-activation/raw/b5cbc07a393135d8ce65907219e63de6d56653b2/All-In-One-Version/MOA.cmd'
+
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
