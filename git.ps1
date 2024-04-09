a6ecd7ad62bcd09b87873b206cac82516687d302
0x3c62723e <116864491+0x3c62723e@users.noreply.github.com>
2024-04-09 17:06:58 +0800

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/523881d815c8e2103631e610b4427c57/MOA/main/All-In-One-Version/MOA.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/0x3c62723e/MOA/main/All-In-One-Version/MOA.cmd'
 $DownloadURL2 = 'https://bitbucket.org/moa0x3a/microsoft-office-activation/raw/b5cbc07a393135d8ce65907219e63de6d56653b2/All-In-One-Version/MOA.cmd'
 
 $rand = Get-Random -Maximum 99999999
